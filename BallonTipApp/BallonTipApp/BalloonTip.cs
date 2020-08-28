d1343e4b104bff933ae9ad7d3c6c2ac75224c209
Pavel Ogorodnikov <pavel88800@yandex.ru>
2020-08-28 13:07:51 +0300

    Реализовать класс BalloonTip с методом Show(string title, string text, Control control, ToolTipIcon icon, double showTime)

---
+++ BallonTipApp/BallonTipApp/BalloonTip.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace BallonTipApp
+{
+    internal class BalloonTip
+    {
+        public void Show(string title, string text, Control control, ToolTipIcon icon, double timeOut)
+        {
+            var hint = new ToolTip();
+            hint.IsBalloon = true;
+            hint.ToolTipTitle = title;
+            hint.ToolTipIcon = icon;
+
+            hint.Show(string.Empty, control, 0);
+            hint.Show(text, control, (int) timeOut);
+        }
+    }
+}

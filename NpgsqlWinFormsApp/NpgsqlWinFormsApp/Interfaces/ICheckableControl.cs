b26a2048fe3e51b2f0493d48065577d9ccbf0168
Pavel Ogorodnikov <pavel88800@yandex.ru>
2020-08-29 17:52:14 +0300

    Реализовать расширяющий метод bool DataCheck() для ControlCollection, в котором будет производиться проверка всех элементов управления на форме с интерфейсом ICheckableControl на заполненность данных в них. Если данные заполнены, то метод должен возвращать true

---
+++ NpgsqlWinFormsApp/NpgsqlWinFormsApp/Interfaces/ICheckableControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpgsqlWinFormsApp.Interfaces
+{
+    interface ICheckableControl
+    {
+        /// <summary>
+        /// Проверять или нет компонент на то, что данные не пустые.
+        /// </summary>
+        bool EmptyDataCheck { get; set; }
+
+
+        /// <summary>
+        /// Проверять на наличие данных в компоненте
+        /// </summary>
+        /// <returns></returns>
+        bool Check();
+    }
+}
